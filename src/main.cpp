b0a101745890faa94c78b018acdbe68d22789e39
SywthWin11 <perera.seth@outlook.com>
2025-11-01 17:49:05 +0000

    this is a working config for windows11

---
+++ src/main.cpp
@@ -0,0 +1,30 @@
+#include "config.h"
+
+int main() {
+    GLFWwindow* window;
+
+    if (!glfwInit()) {
+        std::cout<<"Failed to initialize GLFW" << std::endl;
+        return -1;
+    }
+
+    std::cout << "GLFW initialized successfully" << std::endl;
+    window = glfwCreateWindow(640, 480, "GLFW Window Test", nullptr, nullptr);
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+        std::cout<<"Failed to initialize GLAD" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+
+    glClearColor(0.8f, 0.1f, 0.8f, 1.0f);
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+        glClear(GL_COLOR_BUFFER_BIT);
+        glfwSwapBuffers(window);
+    }
+
+    glfwTerminate();
+    return 0;
+}
